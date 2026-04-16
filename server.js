02995e7841ef3b075202a47989b20217bea7d1f7
Infinity <william.soo1048576@gmail.com>
2026-04-16 00:05:58 +0000

    Modified server.js to handle service worker issue with express etags

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const path = require('path')
 const port = process.env.PORT || 443
 
 const app = express()
+
+app.get('/sw.js', (req, res) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Content-Type', 'application/javascript');
+    res.setHeader('Service-Worker-Allowed', '/');
+    res.sendFile(path.resolve(__dirname, 'sw.js'));
+});
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) =>  {
